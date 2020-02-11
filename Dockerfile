355de4cef15b7eb6a04f3ce076ef510ba8cad1ae
randy brown <thatrandybrown@gmail.com>
2020-02-10 22:58:41 -0600

    generate a uuid for the post and send it to messaging
    because the cache will need to be updated with the newest version of the post,
    it is handled async and then read from cache later. For now, the post is
    accepted as provided.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,10 @@
 FROM dutchoven/express
 
+COPY package*.json /app/
+RUN npm ci
+
+FROM dutchoven/express
+
+# this is dangerous for larger apps due to the chances of collision
+COPY --from=build /app/node_modules ./node_modules
 COPY index.js /app/src/resources/
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import uuid from 'uuidv4';
+
 export default ({config, boundary, logger, messageHandler, entityMapper}) => {
     const {read, write} = entityMapper.define('post', []);
 
@@ -7,6 +9,17 @@ export default ({config, boundary, logger, messageHandler, entityMapper}) => {
             {endpoint: "/", method: "get", behavior: [
                 (req, res, next) => read().then(data => res.send(data))
             ]},
+            {endpoint: "/", method: "post", behavior: [
+                (req, res, next) => {
+                    /**
+                     * probably good to do some data checking on the body
+                     * before sending it on
+                    **/
+                    messageHandler.write(config.messaging.queue,
+                            JSON.stringify({id: uuid(), ...req.body}))
+                    return res.status(202).send({id})
+                }
+            ]}
         ]
     }];
 }
