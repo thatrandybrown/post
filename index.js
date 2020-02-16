d4e92dd2931f9b0d898689b56e83de0075de16fa
randy brown <thatrandybrown@gmail.com>
2020-02-15 23:26:13 -0600

    provide a promise based interface for extracting posts
    because callbacks have issues in the node/express environment, the promisify
    util is leveraged to convert the redis client eallback structure to a promise.
    This has some implications on the structure of what is returned, and whether or
    not failure will cause the promise to throw.

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import {uuid} from 'uuidv4';
 import {createClient} from 'redis';
+import util from 'util';
+const {promisify} = util;
 
 export default ({config, boundary, logger, messageHandler, entityMapper}) => {
-    const client = createClient();
+    const client = createClient("redis://redis:6379");
+    const hgetall = promisify(client.hgetall).bind(client);
 
     return [{
         resource: "/",
         behaviors: [
             {endpoint: "/", method: "get", behavior: [
-                (req, res, next) =>
-                    client.hgetall(
-                        "hosts",
-                        (err,data) => err ? res.send(data) : next(err)
-                    )
+                async (req, res, next) => {
+                    const data = await hgetall("posts");
+                    return res.send(data);
+                }
             ]},
             {endpoint: "/", method: "post", behavior: [
                 (req, res, next) => {
